0adc754552a53ae7d2dfe9db1a0a57d7718f3a37
Colin de Groot <colindegroot2004@gmail.com>
2021-05-10 13:49:53 +0200

    twee test's toegevoegd aan projecten

---
+++ projecten/searchbar/js/script.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+    document.getElementById('frmSearch').onsubmit = function() {
+        window.location = 'http://www.google.com/search?q=site: "HIER KOMT DE SITE" ' + document.getElementById('txtSearch').value;
+        return false;
+    }
+</script>
